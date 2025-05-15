a91086b87929f55574d12d08a6fa77ab01f78eee
Riajul Islam <rjriajul1>
2025-05-15 12:32:17 +0600

    added successfully coffee inside database

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,6 +1,36 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const Update = () => {
+  const handleUpdateForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const coffeeData = Object.fromEntries(formData.entries());
+
+    // create new coffee data base
+    fetch("http://localhost:3000/coffees", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(coffeeData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Your coffee has been saved successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          // console.log('after post ', data);
+          form.reset();
+        }
+      });
+  };
   return (
     <div>
       <div className="lg:px-28 py-16 p-2 bg-base-300 rounded-2xl text-center mt-20">
@@ -15,48 +45,101 @@ const Update = () => {
             distribution of letters, as opposed to using Content here.
           </p>
         </div>
-          <form >
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <form onSubmit={handleUpdateForm}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* coffee name */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl ">Name</label>
-              <input type="text" className="input w-full" placeholder="Coffee Name" />
+              <label className="label text-primary-content font-semibold text-xl ">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                className="input w-full"
+                placeholder="Coffee Name"
+              />
             </fieldset>
 
             {/* coffee chef */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Chef</label>
-              <input type="text" className="input w-full" placeholder="Coffee Chef" />
+              <label className="label text-primary-content font-semibold text-xl">
+                Chef
+              </label>
+              <input
+                type="text"
+                name="chef"
+                className="input w-full"
+                placeholder="Coffee Chef"
+              />
             </fieldset>
             {/* coffee supplier */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Supplier</label>
-              <input type="text" className="input w-full" placeholder="Coffee Supplier" />
+              <label className="label text-primary-content font-semibold text-xl">
+                Supplier
+              </label>
+              <input
+                type="text"
+                name="supplier"
+                className="input w-full"
+                placeholder="Coffee Supplier"
+              />
             </fieldset>
             {/* coffee taste */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Taste</label>
-              <input type="text" className="input w-full" placeholder="Coffee taste" />
+              <label className="label text-primary-content font-semibold text-xl">
+                Taste
+              </label>
+              <input
+                type="text"
+                name="taste"
+                className="input w-full"
+                placeholder="Coffee taste"
+              />
             </fieldset>
             {/* coffee Category  */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Price</label>
-              <input type="text" className="input w-full" placeholder="Coffee price" />
+              <label className="label text-primary-content font-semibold text-xl">
+                Price
+              </label>
+              <input
+                type="text"
+                name="price"
+                className="input w-full"
+                placeholder="Coffee price"
+              />
             </fieldset>
             {/* coffee details */}
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Details</label>
-              <input type="text" className="input w-full" placeholder="Coffee details" />
+              <label className="label text-primary-content font-semibold text-xl">
+                Details
+              </label>
+              <input
+                type="text"
+                name="details"
+                className="input w-full"
+                placeholder="Coffee details"
+              />
             </fieldset>
-        </div>
-           <div className="my-6">
-             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-              <label className="label text-primary-content font-semibold text-xl">Photo</label>
-              <input type="text" className="input w-full" placeholder="Photo URL" />
+          </div>
+          <div className="my-6">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+              <label className="label text-primary-content font-semibold text-xl">
+                Photo
+              </label>
+              <input
+                type="text"
+                name="photo"
+                className="input w-full"
+                placeholder="Photo URL"
+              />
             </fieldset>
-            <input className="bg-primary w-full p-2 border rounded-2xl text-xl mt-6" type="submit" value="Add Coffee" />
-           </div>
-          </form>
+            <input
+              className="bg-primary w-full p-2 border rounded-2xl text-xl mt-6"
+              type="submit"
+              value="Add Coffee"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
